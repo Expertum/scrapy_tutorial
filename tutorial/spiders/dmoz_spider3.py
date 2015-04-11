5695978b8c7cdea35bdea51db38a00b9f70d55b5
Artem <expertum.ave@gmail.com>
2015-04-11 09:57:00 +0300

    scrap links from tutotial Rules

--- tutorial/spiders/dmoz_spider3.py
+++ tutorial/spiders/dmoz_spider3.py
@@ -1,15 +1,17 @@
 import scrapy
-
+from scrapy.contrib.spiders import CrawlSpider, Rule
+from scrapy.contrib.linkextractors import LinkExtractor
 from tutorial.items import DmozItem
 
-class DmozSpider(scrapy.Spider):
+class DmozSpider(CrawlSpider):
     name = "dmoz3"
     allowed_domains = ["dmoz.org"]
-    f = open("urls.txt")
-    start_urls = [url.strip() for url in f.readlines()]
-    f.close()
+    start_urls = ["http://www.dmoz.org/Computers/Programming/Languages"]
 
-    def parse(self, response):
+    rules = (
+        Rule(LinkExtractor(allow=('Languages', )), callback='parse_item'),)
+
+    def parse_item(self, response):
         for sel in response.xpath('//ul/li'):
             item = DmozItem()
             item['title'] = sel.xpath('a/text()').extract()
